38de0907018793a883d9d8133dd9a193061564f5
notisalomon <84035215+notisalomon@users.noreply.github.com>
2025-11-06 23:17:58 +0100

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -32,7 +32,7 @@ const Navigation = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
             <span className="text-2xl font-bold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
-              Labi
+              ELES
             </span>
           </div>
 
