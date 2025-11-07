53a97b125b2a1332df8c277d013c051907c35aec
notisalomon <84035215+notisalomon@users.noreply.github.com>
2025-11-07 02:27:29 +0100

    Updated contact.js

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -21,7 +21,7 @@ exports.handler = async (event, context) => {
     }
 
     const client = new Client({
-      connectionString: process.env.DATABASE_URL, // deine Neon-URL als Env-Var
+      connectionString: process.env.NETLIFY_DATABASE_URL, // deine Neon-URL als Env-Var
       ssl: { rejectUnauthorized: false },
     });
 
